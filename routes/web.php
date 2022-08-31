b37a1e53cf9902a8aa525776bca61c3ce45db89c
lizaa4 <112323838+lizaa4@users.noreply.github.com>
2022-08-31 09:57:23 +0700

    Update web.php
    mengubah web php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php 
+ 
+use App\Http\Controllers\LoginController; 
+use App\Http\Controllers\RegisterController; 
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\PostController; 
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome')
-)};
+Route::get('/dashboard', function () { 
+    return view('index'); 
+});
