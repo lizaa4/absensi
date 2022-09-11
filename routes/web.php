9146d9b9455e3924278f5b745187210df9876377
unknown <nnurhalizameilindai@gmail.com>
2022-09-11 18:35:21 +0700

    mengupdate web

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'index'])->name('login'); 
 Route::post('/login', [LoginController::class, 'login'])->name('login.proses');
 Route::get('/register', [RegisterController::class, 'index'])->name('register'); 
-Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/register', [RegisterController::class, 'register'])->name('register.proses');
 Route::post('/forgot', [LoginController::class, 'forgot'])->name('forgot.proses');
 Route::get('/forgot', [LoginController::class, 'forgot'])->name('forgot');
 Route::get('/reset', [LoginController::class, 'reset'])->name('reset');
