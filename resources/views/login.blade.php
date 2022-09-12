ea683edb423f083657e2630cf79588d9f658fafe
ayasyayas <bintangyasmine18.co.id>
2022-09-12 10:59:36 +0700

    hallo lisa

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -82,29 +82,6 @@
                   <div class="mt-3 d-grid">
                     <button class="btn btn-primary waves-effect waves-light" type="submit">Log In</button>
                   </div>
-
-                  <div class="mt-4 text-center">
-                    <h5 class="font-size-14 mb-3">Sign in with</h5>
-
-                    <ul class="list-inline">
-                      <li class="list-inline-item">
-                        <a href="javascript::void()" class="social-list-item bg-primary text-white border-primary">
-                          <i class="mdi mdi-facebook"></i>
-                        </a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="javascript::void()" class="social-list-item bg-info text-white border-info">
-                          <i class="mdi mdi-twitter"></i>
-                        </a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="javascript::void()" class="social-list-item bg-danger text-white border-danger">
-                          <i class="mdi mdi-google"></i>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-
                   <div class="mt-4 text-center">
                     <a href="forgot" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a>
                   </div>
@@ -117,9 +94,6 @@
 
             <div>
               <p>Don't have an account ? <a href="register" class="fw-medium text-primary"> Signup now </a> </p>
-              <p>© <script>
-                  document.write(new Date().getFullYear())
-                </script> Skote. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
             </div>
           </div>
 
