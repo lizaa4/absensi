2e808c47fe1037400ece5f6df671cfb79afaaa8c
anisasafitri05 <safitrisanisa338@gmail.com>
2022-09-12 14:00:53 +0700

    commit ketiga

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,123 +1,119 @@
-<!doctype html> 
-<html lang="en"> 
- 
-<head> 
-<style> 
-body { 
-    background-image: url('https://i.pinimg.com/originals/48/a0/55/48a055c83aa634df72fbd5b5bcd3fa27.jpg'); 
-    background-repeat: no-repeat;
-    background-attachment: fixed; 
-    background-size: cover;
-    padding-top: 50px;
-    padding-right: 80px;
-    padding-bottom: 50px;
-    padding-left: 80px;
-} 
-</style>
-    <title>Update Data Diri</title> 
-    <!-- Required meta tags --> 
-    <meta charset="utf-8"> 
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
- 
-    <!-- Bootstrap CSS v5.0.2 --> 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> 
- 
-</head> 
- 
-<body> 
-    <div class="container"> 
-        <div class="row"> 
-            <div class="col-md-6 offset-md-3"> 
-                <h1 class="text-center">Update Data Diri</h1> 
-                @if (session('error')) 
-                    <div class="alert alert-danger"> 
-                        {{ session('error') }} 
-                    </div> 
-                @endif 
-                <form action="{{ route('register.proses') }}" method="POST"> 
-                    @csrf 
-                    <div class="form-group"> 
-                        <label for="nama">Access</label> 
-                        <select name="access" id="access"> 
-                            <option value="admin">Admin</option> 
-                            <option value="pembaca">Pembaca</option> 
-                        </select> 
-                        @if ($errors->has('nama')) 
-                            <div class="text-danger"> 
-                                {{ $errors->first('access') }} 
-                            </div> 
-                        @endif 
-                    </div> 
-                     
-                    <div class="form-group"> 
-                        <label for="nama">Email</label> 
-                        <input type="text" class="form-control" id="email" name="email" placeholder="Enter email"> 
-                        @if ($errors->has('email')) 
-                            <div class="text-danger"> 
-                                {{ $errors->first('email') }} 
-                            </div> 
-                        @endif 
-                    </div> 
-                    <div class="form-group"> 
-                        <label for="email">Nama</label> 
-                        <input type="email" class="form-control" id="nama" name="nama" placeholder="Enter nama"> 
-                        @if ($errors->has('nama')) 
-                            <div class="text-danger"> 
-                                {{ $errors->first('nama') }} 
-                            </div> 
-                        @endif 
-                    </div> 
-                    <div class="form-group"> 
-                        <label for="email">Nomor Hp</label> 
-                        <input type="email" class="form-control" id="nomor hp" name="nomor hp" placeholder="Enter nomor hp"> 
-                        @if ($errors->has('nomor')) 
-                            <div class="text-danger"> 
-                                {{ $errors->first('nomor hp') }} 
-                            </div> 
-                        @endif 
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Update | data | diri</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+  <meta content="Themesbrand" name="author" />
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+  <!-- owl.carousel css -->
+  <link rel="stylesheet" href="{{ asset('libs/owl.carousel/assets/owl.carousel.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('libs/owl.carousel/assets/owl.theme.default.min.css') }}">
+  <!-- Bootstrap Css -->
+  <link href="{{ asset('css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+  <!-- Icons Css -->
+  <link href="{{ asset('css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+  <!-- App Css-->
+  <link href="{{ asset('css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <div class="account-pages my-5 pt-sm-5">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-6 col-xl-5">
+          <div class="card overflow-hidden">
+            <div class="bg-primary bg-soft">
+              <div class="row">
+                <div class="col-7">
+                  <div class="text-primary p-4">
+                    <h5 class="text-primary">Welcome !</h5>
+                    <p>Update Data Diri</p>
+                  </div>
                 </div>
-                <select name="perusahaan">
-	<option value="1">TELKOM</option>
-	<option value="2">ISH</option>
-	<option value="3">MITRATEL</option>
-	<option value="4">PST</option>
-	<option value="5">TELKOM AKSES</option>
-    <option value="5">INFOMEDIA</option>
-                </select>
-                    <div class="form-group"> 
-                        <label for="password">Password</label> 
-                        <input type="password" class="form-control" id="password" name="password" 
-                            placeholder="Enter password"> 
-                        @if ($errors->has('password')) 
-                            <div class="text-danger"> 
-                                {{ $errors->first('password') }} 
-                            </div> 
-                        @endif 
-                    </div> 
-                    <style> 
-                        div { 
-                            margin-top: 20px;
-                            margin-bottom: 20px;
-                            margin-right: 0px;
-                            margin-left: 0px;
-                        } 
-                    </style> 
-                    <button type="submit" class="btn btn-primary">Simpan</button> 
-                     
-                     
-                </form> 
-            </div> 
-        </div> 
-    </div> 
- 
-    <!-- Bootstrap JavaScript Libraries --> 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"> 
-    </script> 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" 
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"> 
-    </script> 
-</body> 
- 
+                <div class="col-5 align-self-end">
+                  <img src="{{asset('/images/profile-')}}img.png" alt="" class="img-fluid">
+                </div>
+              </div>
+            </div>
+            <div class="card-body pt-0">
+              <div class="auth-logo">
+                <a href="index.html" class="auth-logo-light">
+                  <div class="avatar-md profile-user-wid mb-4">
+                    <span class="avatar-title rounded-circle bg-light">
+                      <img src="{{asset('/images/logo-lig')}}ht.svg" alt="" class="rounded-circle" height="34">
+                    </span>
+                  </div>
+                </a>
+
+                <a href="index.html" class="auth-logo-dark">
+                  <div class="avatar-md profile-user-wid mb-4">
+                    <span class="avatar-title rounded-circle bg-light">
+                      <img src="{{asset('/images/logo.svg')}}" alt="" class="rounded-circle" height="34">
+                    </span>
+                  </div>
+                </a>
+              </div>
+              <form action="{{ route('register.proses') }}" method="POST">
+              <div class="form-group"> 
+              <div class="p-2">
+                  <div class="mb-3">
+                    <label for="email" class="form-label">email</label>
+                    <input type="text" class="form-control" id="email" placeholder="Masukkan email" required>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="email" class="form-label">nama</label>
+                    <input type="text" class="form-control" id="nama" placeholder="Masukkan nama" required>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="email" class="form-label">nomor hp</label>
+                    <input type="text" class="form-control" id="nomor hp" placeholder="Masukkan nomor hp" required>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="email" class="form-label">email</label>
+                    <input type="text" class="form-control" id="email" placeholder="Masukkan email" required>
+                  </div>
+                  <form action="{{ route('register.proses') }}" method="POST">
+              <div class="form-group"> 
+                        <label for="nama">Perusahaan</label> 
+                        <select name="access" id="access"> 
+                            <option value="Telkom">Telkom</option> 
+                            <option value="Ish">Ish</option>
+                            <option value="Mitratel">Mitratel</option>
+                            <option value="Pst">Pst</option>
+                            <option value="Telkom Akses8">Telkom Akses</option>
+                            <option value="Infomedia">Infomedia</option>
+                            <option value="Lain-Lain">Lain-lain</option>
+                        </select> 
+                 
+
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="remember-check">
+                    <label class="form-check-label" for="remember-check">
+                      Ingat saya
+                    </label>
+                  </div>
+
+                  <div class="mt-3 d-grid">
+                    <button class="btn btn-primary waves-effect waves-light" type="submit">simpan</button>
+                  </div>
+
+                 
+  <!-- JAVASCRIPT -->
+  <script src="{{ asset('libs/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('libs/metismenu/metisMenu.min.js') }}"></script>
+  <script src="{{ asset('libs/simplebar/simplebar.min.js') }}"></script>
+  <script src="{{ asset('libs/node-waves/waves.min.js') }}"></script>
+
+  <!-- App js -->
+  <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
 </html>
