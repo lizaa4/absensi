decf9c7d77fa466e9acdaf9d1e86c5e17c09e1c1
anisstri <anstryna46@gmail.com>
2022-09-12 13:41:10 +0700

    dbhddhuh

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController; 
 
 Route::get('/', function () { 
-    // return view('welcome'); 
+    // return view('update'); 
     return redirect()->to('/login');
 });
 
