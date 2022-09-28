46c73ad8b17b2ab15ae05461f54d00821c345681
anisstri <anstryna46@gmail.com>
2022-09-28 13:14:42 +0700

    mengubah history

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -50,7 +50,7 @@
   <section class="jumbotron text-center">
   <img src="{{asset('/images/profile-')}}img.png" alt="rofile" width="200" />
   <h1 class="display-4">Welcome to History Absensi! </h1>
-  <p class="lead"> sudah melaukan absensi!</p>
+  <p class="lead"> anda sudah melakukan absensi!</p>
   <style>
 table {
   font-family: arial, sans-serif;
@@ -67,6 +67,21 @@ td, th {
 tr:nth-child(even) {
   background-color: #dddddd;
 }
+</style>
+</head>
+<body>
+
+<h2>History Absensi</h2>
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  
+</table>
+
 </style>
 
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="0.8" d="M0,96L34.3,112C68.6,128,137,160,206,149.3C274.3,139,343,85,411,90.7C480,96,549,160,617,160C685.7,160,754,96,823,101.3C891.4,107,960,181,1029,192C1097.1,203,1166,149,1234,117.3C1302.9,85,1371,75,1406,69.3L1440,64L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
